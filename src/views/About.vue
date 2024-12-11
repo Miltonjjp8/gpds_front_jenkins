ef1c071cd95b5cb03b46a90a0560118de86eeed2
Milton Jaimes <20193tn028@utez.edu.mx>
2024-12-10 22:04:30 -0600

    levantar jenkins

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>About</h1>
-      <p>Esta es la página de información.</p>
+      <p>Esta es la página de inicio de informacion.</p>
     </div>
   </template>
   
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 <template>
     <div>
       <h1>Home</h1>
-      <p>Bienvenido a la página de inicio.</p>
+      <p>Bienvenido a la página de informacion.</p>
     </div>
   </template>
   
